f467e88ef7d4e2de3576b20ea3ee27f847d4fe89
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-13 02:11:40 +0500

    Passing function via a props
    Learned how a parent component can pass a function to a child, allowing the child to trigger state updates or actions in the parent. Useful for handling events or communication between components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   const items = ["London", "Paris", "New YorK", "Tokyo", "San Francisco"];
-
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading='Cites' />
+      <ListGroup
+        items={items}
+        heading='Cites'
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,10 +4,12 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>{heading}</h1>
@@ -23,7 +25,10 @@ function ListGroup({ items, heading }: Props) {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
